be1d76022cee82790a583e4455ca1e5a4fc6ef2b
Bruno Frigeri <brunofrigeri@Brunos-MacBook-Pro-2.local>
2022-10-21 09:42:44 -0300

    feat: add linked lists data structure

---
+++ Linked Lists/index.ts
@@ -0,0 +1,94 @@
+interface ILinkedListNode {
+  data: number
+  next: ILinkedListNode | null
+}
+
+class LinkedListNode implements ILinkedListNode {
+  data: number
+  next: ILinkedListNode | null = null
+
+  constructor(data: number) {
+    this.data = data
+    this.next = null
+  }
+}
+
+class LinkedList {
+  head: LinkedListNode | null
+  tail: LinkedListNode | null
+
+  constructor() {
+    this.head = null
+    this.tail = null
+  }
+
+  insert(node: LinkedListNode) {
+    if (this.head === null) {
+      this.head = node
+    } else {
+      if (this.tail) {
+        this.tail.next = node
+      }
+    }
+
+    this.tail = node
+  }
+
+  printData() {
+    let llist = this.head
+
+    while (llist !== null) {
+      console.log(llist.data)
+      llist = llist.next
+    }
+  }
+
+  removeNode() {
+    let llist = this.head
+
+    while (llist !== null) {
+      if (llist?.next?.next === null) {
+        llist.next = null
+      } else {
+        llist = llist.next
+      }
+    }
+  }
+
+  removeNodeByPosition(position: number) {
+    let llist = this.head
+
+    let index = 0
+    while (llist !== null) {
+      if (index === position - 1) {
+        if (llist.next !== null) {
+          llist.next = llist.next.next
+          return
+        }
+      } else {
+        index++
+        llist = llist.next
+      }
+    }
+  }
+}
+
+function main() {
+  const arrayOfNumbers = [20, 30, 60, 40, 33, 78, 63, 120, 108, 97]
+
+  let linkedList = new LinkedList()
+  for (let index = 0; index < arrayOfNumbers.length; index++) {
+    const node = new LinkedListNode(arrayOfNumbers[index])
+    linkedList.insert(node)
+  }
+
+  linkedList.printData()
+  console.log('\n')
+  linkedList.removeNode()
+  linkedList.printData()
+  console.log('\n')
+  linkedList.removeNodeByPosition(1)
+  linkedList.printData()
+}
+
+main()
